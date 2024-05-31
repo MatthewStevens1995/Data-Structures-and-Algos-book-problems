3d76bbe018b0b8075b44e8564a2842f739555cae
unknown <funnykid159@gmail.com>
2024-05-30 19:32:52 -0700

    adding the deque linear data structure

--- Data_Structures_and_Algos_by_Miller_and_Ranum/DataStructures.py
+++ Data_Structures_and_Algos_by_Miller_and_Ranum/DataStructures.py
@@ -7,7 +7,7 @@ class Stack:
     
     """A Stack is a Linear data structure. Its like a list in that it accepts heterogenough elements.
     Its defined by its 'first in,last out' property in that elements can only be added and removed to the stack
-    from the same side. thus older elements are removed before newer elements added"""
+    from the same side. thus older elements are removed before newer elements added. its collections are ordered"""
     
     def __init__(self):
         """Initializes an empty Stack"""
@@ -39,7 +39,7 @@ class Queue:
 
     """A Queue is a like a Stack, except that The side of the list that removes/pops off elements and add elements are different sides.
     So instead of a Stack, which has a 'first in last out' property', A queue has a 'First in First out property. Queue elements can
-    Be hetergenous."""
+    Be hetergenous. its collections are ordered"""
     
     def __init__(self):
         """Initialize an empty stack upon object instantiantion"""
@@ -60,4 +60,33 @@ class Queue:
     def size(self)->int:
         """Returns the number of Items in the Queue"""
         return len(self.queue)
-        
+    
+    
+class Deque:
+    
+    """A Deque, pronouched 'deck' is a linear, ordered data structure.
+    What makes It different than the other two linear Data Structures above
+    is that items can be added and removed from either side of the data structure.
+    so you can add items to be beginning, the end, and remove items from the beginning
+    and end. items can be heterogenous like the other linear data structures."""
+    
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+    
+    def addFront(self,item):
+        self.items.append(item)
+    
+    def addRear(self,item):
+        self.items.append(0,item)
+    
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
